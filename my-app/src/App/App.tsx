c8756a33c4e7f2b26e32664c76d11a62e63f2ca7
Vyacheslav Vorobev <voyzvz@gmail.com>
2022-03-21 17:35:44 +0800

    update filter

--- my-app/src/App/App.tsx
+++ my-app/src/App/App.tsx
@@ -1,66 +1,52 @@
 import React from 'react';
 import OffsetTable from "../components/OffsetTable/OffsetTable";
-import {PaginationProps, TableProps} from "../components/OffsetTable";
-import {LogMsgDao} from "../services/dao";
-import {v4 as uuidv4} from 'uuid';
-import {ITransformedLog} from "../types";
-import {CacheableLogMsgDao} from "../services/dao/LogMsg/CacheableLogMsgDao";
+import { PaginationProps, TableProps } from "../components/OffsetTable";
+import { LogMsgDao } from "../services/dao";
+import { v4 as uuidv4 } from 'uuid';
+import { ITransformedLog } from "../types";
+import { CacheableLogMsgDao } from "../services/dao/LogMsg/CacheableLogMsgDao";
 import WSClient from "../components/WSC/SampleWS";
 
 let dao: LogMsgDao = new CacheableLogMsgDao(new LogMsgDao())
 
 let tableProps: TableProps<ITransformedLog> = {
-    mapFunc: value => (
-        <tr>
-            <td key={value.objId}>{value.objId}</td>
-            <td key={value.content}>{value.content}</td>
-            <td key={uuidv4()}>{value.transformed ? 'Y' : 'N'}</td>
-        </tr>
-    ),
+  mapFunc: value => (
+    <tr>
+      <td key={value.objId}>{value.objId}</td>
+      <td key={value.content}>{value.content}</td>
+      <td key={uuidv4()}>{value.transformed ? 'Y' : 'N'}</td>
+    </tr>
+  ),
 
-    columns: ['objId', 'content', 'transformed']
+  columns: ['objId', 'content', 'transformed']
 }
 
 const msgProps3: PaginationProps = {
-    pagePerApiRequest: 10,
-    tablePageSize: 5
+  pagePerApiRequest: 10,
+  tablePageSize: 5
 }
 
 function App() {
-    return (
-        <>
-            <div>
-                <div className="container">
-                    <nav className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
-                        <a className="navbar-brand" href="#!/main">KSDD ARM</a>
-                        <div className="collapse navbar-collapse" id="navbarCollapse">
-                            <ul className="navbar-nav mr-auto sidenav" id="navAccordion">
-                                <li ng-if="isUserLoggedIn() && !isAdmin()" className="nav-item">
-                                    <a className="nav-link" href="#!/errands_create">main</a>
-                                </li>
-                                <li ng-if="isMaster() || isAdmin()" className="nav-item">
-                                    <a className="nav-link" href="#!/errands_pending">journal</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                    <br/>
-                    <br/>
-                    <div className="row">
-                        {/*<div className="col-sm">*/}
-                        {/*    <WSClient/>*/}
-                        {/*</div>*/}
-                        <div className="col-sm">
-                            <OffsetTable dao={dao}
-                                         paginationProps={msgProps3}
-                                         tableProps={tableProps}/>
-                        </div>
-                    </div>
-                </div>
-                <div/>
+  return (
+    <>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#!/main">KSDD ARM</a>
+        </nav>
+        <div className="container">
+          <div className="row">
+            {/*<div className="col-sm">*/}
+            {/*    <WSClient/>*/}
+            {/*</div>*/}
+            <div className="col-sm">
+              <OffsetTable dao={dao}
+                paginationProps={msgProps3}
+                tableProps={tableProps} />
             </div>
-        </>
-    )
+          </div>
+        </div>
+        <div />
+    </>
+  )
 }
 
 export default App;
--- my-app/src/components/OffsetTable/Filter.tsx
+++ my-app/src/components/OffsetTable/Filter.tsx
@@ -1,38 +1,65 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { SubmitHandler, useForm } from "react-hook-form";
 
 let flt = [] as { key: string, value: string }[]
 
-export const Filter = ({setFilter}: {
-    setFilter: (filter: { key: string, value: string } []) => void
+interface IInput {
+  field: string
+  fieldValue: string
+}
+
+export const Filter = ({ setFilter }: {
+  setFilter: (filter: { key: string, value: string }[]) => void
 }) => {
 
-    const [fil_val, setFilterVal] = useState("")
+  const [fil_val, setFilterVal] = useState("")
 
-    function addValue(evt) {
-        console.log("ADDING EVENT VALUE")
-        let val = evt.target.value
-        setFilterVal(val)
-    }
+  function addValue(evt) {
+    console.log("ADDING EVENT VALUE")
+    let val = evt.target.value
+    setFilterVal(val)
+  }
 
-    function search() {
-        if (fil_val.length > 0)
-            flt.push({key: "obj_id", value: fil_val})
-        setFilter(flt)
-    }
+  function search() {
+    if (fil_val.length > 0)
+      flt.push({ key: "obj_id", value: fil_val })
+    setFilter(flt)
+  }
 
-    function clear() {
-        flt = []
-    }
+  function clear() {
+    flt = []
+  }
 
-    return (
-        <>
-            <input type="text" name={"obj_id"} onChange={addValue}/>
-            <div className={"btn btn-primary"} onClick={() => search()}>search</div>
-            <div className={"btn btn-primary"} onClick={() => {
-                clear();
-                search()
-            }}>clear
-            </div>
-        </>
-    )
+  const { register, handleSubmit } = useForm<IInput>();
+
+  const onSubmit:SubmitHandler<IInput> = (data) => {
+    flt = [{key: "obj_id", value: data.field}]
+    setFilter(flt)
+  }
+
+
+  return (
+    <>
+      <div className={"btn btn-primary"} onClick={() => search()}>search</div>
+      <div className={"btn btn-primary"} onClick={() => {
+        clear();
+        search()
+      }}>clear
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">@</span>
+          </div>
+          <input {...register("field")} />
+        </div>
+
+        <input type="submit" />
+      </form>
+
+    </>
+  )
 }
